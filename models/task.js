619a41c1f4441335c8fc941ceb73a30fb7b17df8
gabrielfs7 <gabrielfs7@gmail.com>
2019-04-01 22:02:47 +0200

    add timestamp when save or update db record

--- models/task.js
+++ models/task.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-    task: {
-        type: String,
-        required: true
+const taskSchema = new Schema(
+    {
+        task: {
+            type: String,
+            required: true
+        },
+        doAt: {
+            type: Date,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    doAt: {
-        type: Date,
-        required: true
-    },
-    status: {
-        type: String,
-        required: true
-    },
-    owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+    {
+        timestamps: true
     }
-});
+);
 
 module.exports = mongoose.model('Task', taskSchema);
--- models/user.js
+++ models/user.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true
+const userSchema = new Schema(
+    {
+        email: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        birthDate: {
+            type: Date,
+            required: true
+        },
+        tasks: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Task'
+            }
+        ]
     },
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    birthDate: {
-        type: Date,
-        required: true
-    },
-    tasks: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Task'
-        }
-    ]
-});
+    {
+        timestamps: true
+    }
+);
 
 module.exports = mongoose.model('User', userSchema);
