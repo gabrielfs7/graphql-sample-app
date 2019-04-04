fe9eb17029958dbec5432998396a8337f78c1499
gabrielfs7 <gabrielfs7@gmail.com>
2019-04-04 21:59:26 +0200

    clean code. create normalize for tasks

--- services/task/FindTaskService.js
+++ services/task/FindTaskService.js
@@ -3,7 +3,6 @@ class FindTaskService
     findById() {
         return async (taskId) => {
             try {
-                const findUserService = require('../user/FindUserService');
                 const taskModel = require('../../models/task');
                 const task = await taskModel.findById(taskId);
 
@@ -11,12 +10,7 @@ class FindTaskService
                     throw new Error('Task ' + taskId + ' not found');
                 }
 
-                return {
-                    ...task._doc,
-                    _id: task._doc._id.toString(),
-                    doAt: task._doc.doAt.toLocaleDateString(),
-                    owner: findUserService.findById().bind(this, task._doc.owner)
-                }
+                return this.normalize(task);
             } catch (err) {
                 throw err;
             }
@@ -26,19 +20,11 @@ class FindTaskService
     findByIds() {
         return async (taskIds) => {
             try {
-                const findUserService = require('../user/FindUserService');
                 const taskModel = require('../../models/task');
 
                 const tasks = await taskModel.find({ _id: { $in: taskIds } });
 
-                return tasks.map(task => {
-                    return {
-                        ...task._doc,
-                        _id: task._doc._id.toString(),
-                        doAt: task._doc.doAt.toLocaleDateString(),
-                        owner: findUserService.findById().bind(this, task._doc.owner)
-                    }
-                });
+                return this.normalizeCollection(tasks);
             } catch (err) {
                 throw err;
             }
@@ -48,24 +34,35 @@ class FindTaskService
     findAll() {
         const findTasks = async () => {
             try {
-                const findUserService = require('../user/FindUserService');
                 const taskModel = require('../../models/task');
                 const tasks = await taskModel.find();
     
-                return tasks.map(task => {
-                    return {
-                        ...task._doc,
-                        _id: task.id, 
-                        doAt: task._doc.doAt.toLocaleDateString(),
-                        owner: findUserService.findById().bind(this, task._doc.owner)
-                    }
-                });
+                return this.normalizeCollection(tasks);
             } catch (err) {
                 throw err;
             }
         }
 
         return findTasks();
+    }
+
+    normalize(task)
+    {
+        const findUserService = require('../user/FindUserService');
+
+        return {
+            ...task._doc,
+            _id: task._doc._id.toString(),
+            doAt: task._doc.doAt.toLocaleDateString(),
+            owner: findUserService.findById().bind(this, task._doc.owner)
+        }
+    }
+
+    normalizeCollection(tasks)
+    {
+        return tasks.map(task => {
+            return this.normalize(task);
+        });
     }
 }
 
