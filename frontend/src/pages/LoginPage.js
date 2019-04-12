9578e6267c128900706a369feded7d23ff2dc726
gabrielfs7 <gabrielfs7@gmail.com>
2019-04-12 14:27:09 +0200

    Addind login page and styles

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import './LoginPage.css';
 
 class LoginPage extends Component {
     render() {
-        return <h1>Login Page</h1>;
+        return (
+            <form className="login-form">
+                <div className="form-control">
+                    <label htmlFor="email">E-mail</label>
+                    <input type="text" id="email" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className="form-actions">
+                    <button type="submit">Login</button>
+                    <button type="button">Switch to Signup</button>
+                </div>
+            </form>
+        );
     }
 }
 
